166e7e36ed0cd7d8741f3ff1a1c123b0890f1e39
Biraj Silwal <birajsilwal.ai@gmail.com>
2022-10-06 23:34:25 -0500

    computer player | stop game immediately once winner is declared

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,7 +1,11 @@
+import random
+
+
 BOARD = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
 GAME_IS_RUNNING = True
 WINNER = None
 CURRENT_PLAYER = 'X'
+got_winner = False
 
 
 # display the board
@@ -15,8 +19,8 @@ def display_board(board):
 
 # take user input and update board value
 def take_input(board):
-    user_input = int(input("Enter value from 0-8: "))
-    board[user_input] = CURRENT_PLAYER
+    user_input = int(input("Enter value from 1-9: "))
+    board[user_input - 1] = CURRENT_PLAYER
 
 
 # check row
@@ -68,6 +72,18 @@ def check_tie(board):
         GAME_IS_RUNNING = False
 
 
+# check for winner
+def check_winner(board):
+    global GAME_IS_RUNNING
+    global got_winner
+    if check_row(board) or check_col(board) or check_diag(board):
+        print("*******")
+        print('Winner is ' + CURRENT_PLAYER) 
+        display_board(board)
+        GAME_IS_RUNNING = False
+        got_winner = True
+
+
 # switch user
 def switch_user():
     global CURRENT_PLAYER
@@ -76,25 +92,37 @@ def switch_user():
     else: 
         CURRENT_PLAYER = 'X'
         
+
+# computer's turn
+def computer_player(board):
+    while CURRENT_PLAYER == 'O':
+        num = random.randint(0, 8)
+        if board[num] == '-':
+            board[num] = 'O'
+            switch_user()  # switch to human, this must be here to get out of while loop
+        
         
 # play game
 def play():
     global GAME_IS_RUNNING
     global BOARD
-    while GAME_IS_RUNNING:
+    global  WINNER
+    global got_winner
+    while GAME_IS_RUNNING:    
         display_board(BOARD)
-        take_input(BOARD)
-        if check_row(BOARD) or check_col(BOARD) or check_diag(BOARD):
-            display_board(BOARD)
-            print('Winner is ' + CURRENT_PLAYER)
-            GAME_IS_RUNNING = False
-        else:
-            check_tie(BOARD)
-            switch_user()
         
-
+        if not got_winner:
+            take_input(BOARD) # take input from user
+            check_winner(BOARD)
+            check_tie(BOARD)
+            switch_user()  # switch to computer
+            
+        if not got_winner:
+            computer_player(BOARD)
+            check_winner(BOARD)
+            check_tie(BOARD)
+        
 
 if __name__ == '__main__':
     print("Let's begin the game!\n")
     play()
-
